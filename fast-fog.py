ec762e86944af07315d126bde5a83ec77f6ebd7a
WhiteBowlerHat <maxime.rouhier.github@gmail.com>
2021-12-23 17:35:08 +0100

    Adding faster fog project

--- fast-fog.py
+++ fast-fog.py
@@ -38,7 +38,7 @@ def safe_shuffle(key,list):
         list[i],list[j] = list[j],list[i]
 
 
-def choose_pixels(key,list, nb):
+def choose_pixels(key,list,nb):
     m = hashlib.sha256()
     m.update(key.encode('utf-8'))
     seed = m.digest() # use SHA-256 to hash different size seeds
@@ -53,6 +53,22 @@ def choose_pixels(key,list, nb):
         list.remove(list[elem])
         j=j-1
     return array
+
+def safe_randrange(key, bank_size,nb):
+    m = hashlib.sha256()
+    m.update(key.encode('utf-8'))
+    seed = m.digest() # use SHA-256 to hash different size seeds
+    nonce_rfc7539 = bytes([0x00]) * 12
+    cipher = ChaCha20.new(key=seed, nonce=nonce_rfc7539)
+    zerobuf = bytes([0x00]) * 5
+    array = []
+    for i in range(nb):
+        elem = sample(bank_size,cipher,zerobuf)
+        array.append(elem)
+    return array
+
+def decode_binary_string(s):
+    return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
 
 def vers_8bit(c):
 	chaine_binaire = bin(ord(c))[2:]
@@ -73,7 +89,7 @@ def recuperer_bit_pfaible(pixel):
 def extract_message(image_file,taille,key):
    message = ""
    image =Image.open(image_file)
-   array = the_shuffle(image,key)
+   array = the_shuffle(image,key,taille)
    dimX,dimY = image.size
    im = image.load()
    posx_pixel = 0
@@ -81,20 +97,18 @@ def extract_message(image_file,taille,key):
    i=0
    for rang_car in range(0,taille):
       rep_binaire = ""
-      for rang_bit in range(0,8):
-         posy_pixel, posx_pixel = divmod(array[i], dimX)
-         rep_binaire += recuperer_bit_pfaible(im[posx_pixel,posy_pixel])
-         i+=1
-      message += chr(int(rep_binaire, 2))
+      posy_pixel, posx_pixel = divmod(array[i], dimX)
+      rep_binaire += recuperer_bit_pfaible(im[posx_pixel,posy_pixel])
+      i+=1
+      message += rep_binaire#chr(int(rep_binaire, 2))
    image.close()
    return message
 
-def hide(image_file,message,order,key):
+def hide(image_file,message_binaire,order,key):
    image = Image.open(image_file)
+   array = the_shuffle(image,key,len(message_binaire))
    dimX,dimY = image.size
-   array = the_shuffle(image,key)
    im = image.load()
-   message_binaire = ''.join([vers_8bit(c) for c in message])
    metadata = PngInfo()
    metadata.add_text("Order", str(order))
    i=0
@@ -105,23 +119,21 @@ def hide(image_file,message,order,key):
    return metadata,image
 
 def split_msg(msg,key,nb):
-   image_nb = [random.randrange(0,nb) for i in range(len(msg))]
+   image_nb =  safe_randrange(key, nb, len(msg))
    return image_nb
 
 def split_msg2(taille,key,nb):
-
-   image_nb = [random.randrange(0,nb) for i in range(taille)]
+   image_nb = safe_randrange(key, nb, taille)
    return image_nb
 
-def the_shuffle(img,key):
+def the_shuffle(img,key,lenmsg):
    dimX,dimY = img.size
    length =  dimX*dimY
    array = []
    for i in range(length):
       array.append(i)
-   safe_shuffle(key,array)
-   print(array[0])
-   return array
+   arr = choose_pixels(key,array,lenmsg)
+   return arr
 
 def smallf(x,arr,msg):
    string =""
@@ -133,10 +145,12 @@ def smallf(x,arr,msg):
 def fog(key,message,image_bank):
    random.seed(key)
    img_list=glob.glob(image_bank+'/*.png')
-   image_nb = split_msg(message,key,len(img_list))
+   message_binaire = ''.join([vers_8bit(c) for c in message])
+   print(len(message_binaire))
+   image_nb = split_msg(message_binaire,key,len(img_list))
    splitted_msg =[]
    for i in range(len(img_list)):
-      splitted_msg.append(smallf(i,image_nb,message))
+      splitted_msg.append(smallf(i,image_nb,message_binaire))
    for idx,i in enumerate(img_list):
       if splitted_msg[idx] != '':
          metadata,image = hide(i,splitted_msg[idx],idx,key)
@@ -157,7 +171,8 @@ def fog(key,message,image_bank):
 def wind(key,size,directory):
    random.seed(key)
    img_list=glob.glob(directory+'/*.png-fog')
-   img_nb=split_msg2(size,key,len(img_list))
+   print(size)
+   img_nb=split_msg2(size,key,7)
    arr_size = []
    sorted_img_list = [0]*len(img_list)
    for i in range(len(img_list)):
@@ -174,26 +189,28 @@ def wind(key,size,directory):
          msg_array_unordered.append(extract_message(i,arr_size[idx],key))
       else:
          msg_array_unordered.append("")
-   content=""
+   bcontent=""
    for i in range(len(img_nb)):
       s = msg_array_unordered[img_nb[i]]
-      content+=s[0]
+      bcontent+=s[0]
       msg_array_unordered[img_nb[i]]=s[1:]
-   print(content)
+   content = decode_binary_string(bcontent)
+   print("Hidden msg : "+content+"\n")
 
 
-# print("Starting encryption...")
-# fog("key2","maximus premierp","bank")
-# print("Encryption ended successfully ! Images are stored in the 'fog' directory !")
-# print("Starting decryption...")
-# wind("key2",16,"fog")
-img = Image.open('bank/suda.png')
-dimX,dimY = img.size
-length =  dimX*dimY
-array = []
-for i in range(length):
-    array.append(i)
-pixel_array = choose_pixels("lmfaoooooo",array,3)
-for i in pixel_array:
-   posy_pixel, posx_pixel = divmod(i, dimX)
-   print("X :"+str(posy_pixel)+"; Y:"+str(posx_pixel))
+#print("Starting encryption...")
+#fog("key2","maximus premierp","bank")
+#print("Encryption ended successfully ! Images are stored in the 'fog' directory !")
+print("Starting decryption...")
+wind("key2",128,"fog")
+# img = Image.open('bank/suda.png')
+# dimX,dimY = img.size
+# length =  dimX*dimY
+# array = []
+# for i in range(length):
+#     array.append(i)
+# pixel_array = choose_pixels("lmfaoooooo",array,6)
+# for i in pixel_array:
+#    posy_pixel, posx_pixel = divmod(i, dimX)
+#    print("X :"+str(posy_pixel)+"; Y:"+str(posx_pixel))
+# print(safe_randrange("lmfaooooo",7,6))
