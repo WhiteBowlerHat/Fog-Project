1ab35a25326cb12b50d0ede27408a578a5b94684
WhiteBowlerHat <maxime.rouhier.github@gmail.com>
2022-04-14 15:16:32 +0200

    Cleaning the code

--- fast-fog.py
+++ fast-fog.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from PIL import Image
 from PIL.PngImagePlugin import PngImageFile, PngInfo
 import glob
@@ -10,6 +11,9 @@ import zipfile
 import io
 
 
+
+# Function used for safe randomization
+# Taken here -> https://stackoverflow.com/questions/66528995/cryptographically-secure-pseudo-random-shuffle-a-list-or-array-in-python
 def sample(max,cipher,zerobuf):
     stream_size = (max.bit_length() + 2 + 7) // 8
     max_stream_value = 1 << (stream_size * 8)
@@ -21,6 +25,7 @@ def sample(max,cipher,zerobuf):
             break
     return candidate % max
 
+# Function that return an array (size "size") of "random" number between 0 - nb
 def safe_randrange(nb,size,cipher):
     zerobuf = bytes([0x00]) * 5
     array = []
@@ -29,34 +34,29 @@ def safe_randrange(nb,size,cipher):
         array.append(elem)
     return array
 
-def decode_binary_string(s):
-    return ''.join(chr(int(s[i*16:i*16+16],2)) for i in range(len(s)//16))
 
-def vers_8bit(c):
-	chaine_binaire = bin(ord(c))[2:]
-	return "0"*(16-len(chaine_binaire))+chaine_binaire
 
-def modifier_pixel(pixel, bit):
-	r_val = pixel[0]
-	rep_binaire = bin(r_val)[2:]
-	rep_bin_mod = rep_binaire[:-1] + bit
-	r_val = int(rep_bin_mod, 2)
-	return tuple([r_val] + list(pixel[1:]))
+# --- Function only useful for string encryption, not zip files
+# def decode_binary_string(s):
+#     return ''.join(chr(int(s[i*16:i*16+16],2)) for i in range(len(s)//16))
 
-def recuperer_bit_pfaible(pixel):
+# def vers_8bit(c):
+# 	chaine_binaire = bin(ord(c))[2:]
+# 	return "0"*(16-len(chaine_binaire))+chaine_binaire
+def retrieve_last_bit(pixel):
 	r_val = pixel[0]
 	return bin(r_val)[-1]
 
-def split_msg(taille,nb,cipher):
-   image_nb = safe_randrange(nb, taille, cipher)
+def split_msg(size,nb,cipher):
+   image_nb = safe_randrange(nb, size, cipher)
    return image_nb
 
-def pos_arr_builder(img,lenmsg,cipher):
-   dimX,dimY = img.size
-   length =  dimX*dimY
+def pos_arr_builder(img,image_name,lenmsg,cipher):
+   length = img.size[0]*img.size[1]
    array = []
    zerobuf = bytes([0x00]) * 5
    for i in range(lenmsg):
+      #print("Image : "+str(image_name)+" - "+str(i)+" : Maxime est un gros bg !\n")
       if len(array)== 0:
          array.append(sample(length,cipher,zerobuf))
          length-=1
@@ -69,19 +69,39 @@ def pos_arr_builder(img,lenmsg,cipher):
          array.append(e)
    return array
 
-def extract_message(image_file,taille,cipher):
+# Function that modify the last bit of the red pixel value according
+def modify_pixel(pixel, bit):
+	r_val = pixel[0]
+	rep_binaire = bin(r_val)[2:]
+	rep_bin_mod = rep_binaire[:-1] + bit
+	r_val = int(rep_bin_mod, 2)
+	return tuple([r_val] + list(pixel[1:]))
+
+def hide(image_file,binary_message,cipher):
+   image = Image.open(image_file)
+   array = pos_arr_builder(image,image_file,len(binary_message),cipher)
+   dimX,dimY = image.size
+   im = image.load()
+   i=0
+   for bit in binary_message:
+      posy_pixel, posx_pixel = divmod(array[i], dimX)
+      im[posx_pixel,posy_pixel] = modify_pixel(im[posx_pixel,posy_pixel],bit)
+      i += 1
+   return image
+
+def extract_message(image_file,size,cipher):
    message = ""
    image =Image.open(image_file)
-   array = pos_arr_builder(image,taille,cipher)
+   array = pos_arr_builder(image,image_file,size,cipher)
    dimX,dimY = image.size
    im = image.load()
    posx_pixel = 0
    posy_pixel = 0
    i=0
-   for rang_car in range(0,taille):
+   for rang_car in range(0,size):
       rep_binaire = ""
       posy_pixel, posx_pixel = divmod(array[i], dimX)
-      rep_binaire += recuperer_bit_pfaible(im[posx_pixel,posy_pixel])
+      rep_binaire += retrieve_last_bit(im[posx_pixel,posy_pixel])
       i+=1
       message += rep_binaire
    image.close()
@@ -95,17 +115,7 @@ def max_size(list):
       image.close()
    return size
 
-def hide(image_file,message_binaire,cipher):
-   image = Image.open(image_file)
-   array = pos_arr_builder(image,len(message_binaire),cipher)
-   dimX,dimY = image.size
-   im = image.load()
-   i=0
-   for bit in message_binaire:
-      posy_pixel, posx_pixel = divmod(array[i], dimX)
-      im[posx_pixel,posy_pixel] = modifier_pixel(im[posx_pixel,posy_pixel],bit)
-      i += 1
-   return image
+
 
 def smallf(x,arr,msg):
    string =""
@@ -138,12 +148,12 @@ def fog(key,message,image_bank,destination_folder):
    print(snonce)
    print(max_size(img_list))
    cipher = ChaCha20.new(key=seed, nonce=nonce_rfc7539)
-   message_binaire = zip_to_bytestring(message)#''.join([vers_8bit(c) for c in message])
-   print(len(message_binaire))
-   image_nb = split_msg(len(message_binaire),len(img_list),cipher)
+   binary_message = zip_to_bytestring(message)#''.join([vers_8bit(c) for c in message])
+   print(len(binary_message))
+   image_nb = split_msg(len(binary_message),len(img_list),cipher)
    splitted_msg =[]
    for i in range(len(img_list)):
-      splitted_msg.append(smallf(i,image_nb,message_binaire))
+      splitted_msg.append(smallf(i,image_nb,binary_message))
    for idx,i in enumerate(img_list):
       if splitted_msg[idx] != '':
          image = hide(i,splitted_msg[idx],cipher)
@@ -164,6 +174,7 @@ def fog(key,message,image_bank,destination_folder):
          savepath = destination_folder+savepath[len(image_bank):]
          image.save(savepath, "png", pnginfo=metadata)
          image.close()
+   print(len(binary_message))
 
 def wind(key,size,directory):
    img_list=glob.glob(directory+'/*.png-fog')
@@ -206,18 +217,13 @@ def wind(key,size,directory):
 import time
 start_time = time.time()
 print("Starting encryption...")
-fog("key2","requirements.zip","bank","fog")
+fog("key2","hello.zip","bank","fog")
 print("--- %s seconds ---" % (time.time() - start_time))
 print("Encryption ended successfully ! Images are stored in the 'fog' directory !")
+binary_message = zip_to_bytestring('capt.zip')#''.join([vers_8bit(c) for c in message])
+print(len(binary_message))
 inp = input("Size ? : ")
 start_time = time.time()
 print("Starting decryption...")
 wind("key2",int(inp),"fog")
 print("--- %s seconds ---" % (time.time() - start_time))
-
-# t=zip_to_bytestring("hello.zip")
-# z=bytestring_to_zip(t)
-# in_memory = io.BytesIO(z)
-# data = in_memory.read()
-# with open(path+'.zip','wb') as out:
-#       out.write(data)c
