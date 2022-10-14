37d10d5155fbc74fa89911b1139396c5fe97f67c
WhiteBowlerHat <95751228+WhiteBowlerHat@users.noreply.github.com>
2022-10-14 11:11:28 +0200

    Cleanin code
    - Added comment
    - Deleted unecessary elements

--- fast-fog.py
+++ fast-fog.py
@@ -34,22 +34,9 @@ def safe_randrange(nb,size,cipher):
         array.append(elem)
     return array
 
-
-
-# --- Function only useful for string encryption, not zip files
-# def decode_binary_string(s):
-#     return ''.join(chr(int(s[i*16:i*16+16],2)) for i in range(len(s)//16))
-
-# def vers_8bit(c):
-# 	chaine_binaire = bin(ord(c))[2:]
-# 	return "0"*(16-len(chaine_binaire))+chaine_binaire
 def retrieve_last_bit(pixel):
 	r_val = pixel[0]
 	return bin(r_val)[-1]
-
-def split_msg(size,nb,cipher):
-   image_nb = safe_randrange(nb, size, cipher)
-   return image_nb
 
 def pos_arr_builder(img,image_name,lenmsg,cipher):
    length = img.size[0]*img.size[1]
@@ -69,7 +56,7 @@ def pos_arr_builder(img,image_name,lenmsg,cipher):
          array.append(e)
    return array
 
-# Function that modify the last bit of the red pixel value according
+# -- Function that modify the last bit of the red pixel value according to a bit --
 def modify_pixel(pixel, bit):
 	r_val = pixel[0]
 	rep_binaire = bin(r_val)[2:]
@@ -77,6 +64,7 @@ def modify_pixel(pixel, bit):
 	r_val = int(rep_bin_mod, 2)
 	return tuple([r_val] + list(pixel[1:]))
 
+# -- Function that hide content in one images --
 def hide(image_file,binary_message,cipher):
    image = Image.open(image_file)
    array = pos_arr_builder(image,image_file,len(binary_message),cipher)
@@ -89,6 +77,7 @@ def hide(image_file,binary_message,cipher):
       i += 1
    return image
 
+# -- Function that retrieve the hidden content in one images -- 
 def extract_message(image_file,size,cipher):
    message = ""
    image =Image.open(image_file)
@@ -107,6 +96,7 @@ def extract_message(image_file,size,cipher):
    image.close()
    return message
 
+# -- Function that add the size of each images-- 
 def max_size(list):
    size=0
    for i in list:
@@ -115,6 +105,7 @@ def max_size(list):
       image.close()
    return size
 
+# -- Function that create string for each x present in a list -- 
 def smallf(x,arr,msg):
    string =""
    for i in range(len(arr)):
@@ -122,6 +113,7 @@ def smallf(x,arr,msg):
          string+=msg[i]
    return string
 
+# -- Function that convert a zip to bytestring --
 def zip_to_bytestring(path):
    tst=""
    with open(path, "rb") as f:
@@ -129,6 +121,7 @@ def zip_to_bytestring(path):
       tst="".join(["{:08b}".format(x) for x in bytes])
    return tst
 
+# -- Function that convert a bytestring to zip --
 def bytestring_to_zip(s,path):
    z=int(s,2).to_bytes(int(len(s)/8),byteorder="big")
    in_memory = io.BytesIO(z)
@@ -136,6 +129,7 @@ def bytestring_to_zip(s,path):
    with open(path+'.zip','wb') as out:
       out.write(data)
 
+# -- Encrypting funtion -- 
 def fog(key,message,image_bank,destination_folder):
    # Retrieve all images
    img_list=glob.glob(image_bank+'/*.png')
@@ -146,7 +140,7 @@ def fog(key,message,image_bank,destination_folder):
    seed = m.digest() # use SHA-256 to hash different size seeds
 
    # Generate a nonce according to rfc8439 (Obsoletes 7539)
-   nonce_rfc7539 = get_random_bytes(24)
+   nonce_rfc7539 = get_random_bytes(24) # XChacha20 nonce 192 bits
    snonce = b64encode(nonce_rfc7539).decode('utf-8')
 
    # --- Debug ---
@@ -154,7 +148,7 @@ def fog(key,message,image_bank,destination_folder):
    print(max_size(img_list))
    # --- End Debug ---
 
-   # Generate Chacha20 python object with nonce and key 
+   # Generate XChacha20 python object with nonce and key 
    cipher = ChaCha20.new(key=seed, nonce=nonce_rfc7539)
 
    # Retrieve all bytes of the zipfile
@@ -162,7 +156,7 @@ def fog(key,message,image_bank,destination_folder):
    print(len(binary_message))
    
    # Split bytes according to the number of images
-   image_nb = split_msg(len(binary_message),len(img_list),cipher)
+   image_nb = safe_randrange(len(binary_message),len(img_list),cipher)
    splitted_msg =[]
    for i in range(len(img_list)):
       splitted_msg.append(smallf(i,image_nb,binary_message))
@@ -191,19 +185,29 @@ def fog(key,message,image_bank,destination_folder):
 
    # Print the binary message length for decryption
    print(len(binary_message))
-
+	
+# -- Decrypting function -- 
 def wind(key,size,directory):
+   # Retrieve all images
    img_list=glob.glob(directory+'/*.png-fog')
    print(size)
+    
+   # Encode key with SHA-256
    m = hashlib.sha256()
    m.update(key.encode('utf-8'))
-   seed = m.digest() # use SHA-256 to hash different size seeds
-   # --TO VERIFY-- 
-   # nonce_rfc7539 = seed & 0xffffffffffff
+   seed = m.digest()
+	
+   # Retrieve nonce from image metadata
    targetImage = PngImageFile(img_list[0])
    nonce_rfc7539 = b64decode(targetImage.text["Nonce"])
+
+   # Generate XChacha20 python object from nonce and key
    cipher = ChaCha20.new(key=seed, nonce=nonce_rfc7539)
-   img_nb=split_msg(size,len(img_list),cipher)
+
+   # Get array of picture number 
+   img_nb=safe_randrange(size,len(img_list),cipher)
+   
+   # Split list according to images number
    arr_size = []
    sorted_img_list = [0]*len(img_list)
    for i in range(len(img_list)):
@@ -214,6 +218,8 @@ def wind(key,size,directory):
          if k==i:
             j+=1
       arr_size.append(j)
+
+   #Retrieve content in each images
    msg_array_unordered=[]
    for idx,i in enumerate(sorted_img_list):
       if arr_size[idx] != 0:
@@ -221,23 +227,23 @@ def wind(key,size,directory):
       else:
          msg_array_unordered.append("")
    bcontent=""
+
+   # Append content of each images
    for i in range(len(img_nb)):
       s = msg_array_unordered[img_nb[i]]
       bcontent+=s[0]
       msg_array_unordered[img_nb[i]]=s[1:]
-   # content = decode_binary_string(bcontent)
-   # print("Hidden msg : "+content+"\n")
+
+   #Convert byte content to zip
    bytestring_to_zip(bcontent,"out")
 
-
+# -- Main script to assess time consumption of each function --
 import time
 start_time = time.time()
 print("Starting encryption...")
 fog("key2","hello.zip","bank","fog")
 print("--- %s seconds ---" % (time.time() - start_time))
 print("Encryption ended successfully ! Images are stored in the 'fog' directory !")
-binary_message = zip_to_bytestring('capt.zip')#''.join([vers_8bit(c) for c in message])
-print(len(binary_message))
 inp = input("Size ? : ")
 start_time = time.time()
 print("Starting decryption...")
